1a8973009f12df672016bba40ced3e4fe6dcd676
allthatjava <allthatjava@yahoo.ca>
2017-11-23 18:27:07 -0500

    Update CustomerRepoTest with fixed port

--- consumer/src/test/java/brian/boot/example/cloud/contract/consumer/repository/CustomerRepoTest.java
+++ consumer/src/test/java/brian/boot/example/cloud/contract/consumer/repository/CustomerRepoTest.java
@@ -2,24 +2,34 @@ package brian.boot.example.cloud.contract.consumer.repository;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.HttpClientErrorException;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureStubRunner(ids = "brian.boot.example.cloud.contract:producer:0.0.1-SNAPSHOT:stubs", workOffline=true)
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
+@AutoConfigureStubRunner(ids = "brian.boot.example.cloud.contract:producer:+:stubs:8080", workOffline=true)
 // Port number not set - Using random port for testing
 // Used specific version 0.0.1-SNAPSHOT - if test must be done against latest, put + symbol instead of the version
 public class CustomerRepoTest {
 
 	@Autowired
 	CustomerRepo repo;
+	
+//	@Value("${stubrunner.runningstubs.producer.port}") int producerPort;
+//
+//	@Before
+//	public void setupPort() {
+//		repo.port = producerPort;
+//	}
 	
 	/**
 	 * For the contract restGetMultipleCustomerContract.groovy
