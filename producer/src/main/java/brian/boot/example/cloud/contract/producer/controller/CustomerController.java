9066ec442ef1276a278bd7ca930780035fc3e061
allthatjava <allthatjava@yahoo.ca>
2017-11-26 21:10:45 -0500

    Clean up the test code

--- producer/src/main/java/brian/boot/example/cloud/contract/producer/controller/CustomerController.java
+++ producer/src/main/java/brian/boot/example/cloud/contract/producer/controller/CustomerController.java
@@ -1,12 +1,11 @@
 package brian.boot.example.cloud.contract.producer.controller;
 
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,39 +15,35 @@ import org.springframework.web.bind.annotation.RestController;
 
 import brian.boot.example.cloud.contract.producer.model.Customer;
 import brian.boot.example.cloud.contract.producer.model.CustomerResponse;
+import brian.boot.example.cloud.contract.producer.service.CustomerService;
 
 @RestController
 public class CustomerController {
+	
+	CustomerService customerService;
+	
+	@Autowired
+	public CustomerController(CustomerService customerService)
+	{
+		this.customerService = customerService;
+	}
 
 	@RequestMapping( value="/customer/{custId}", method = RequestMethod.GET )
 	public Customer getCustomer(@PathVariable("custId") String custId){
 		
-		return new Customer( custId, "John", "Smith", 20);
+		return customerService.getCustomer(custId);
 	}
 	
 	@RequestMapping( value="/customer", method = RequestMethod.POST)
 	public CustomerResponse createCustomer(@RequestBody Customer customer) {
 		
-		return new CustomerResponse( "OK", "Customer created");
+		return customerService.createCustomer(customer);
 	}
 	
 	@RequestMapping( value="/multiple-contract-customer/{custId}", method = RequestMethod.GET)
 	public Customer getMultipleContractCustomer(@PathVariable("custId") String custId){
 		
-		Pattern pattern = Pattern.compile("[0-9]{3}");
-        Matcher matcher = pattern.matcher(custId);
-        
-        if( !matcher.matches() )
-        	throw new IllegalArgumentException("Customer ID is not numeric type");
-		
-		
-		int age = -1;
-		if( "123".equals(custId))
-			age = 20;
-		else if ( "456".equals(custId))
-			age = 30;
-		
-		return new Customer( custId, "John", "Smith", age);
+		return customerService.getMultipleContractCustomer(custId);
 	}
 	
     @ExceptionHandler
--- producer/src/test/java/brian/boot/example/cloud/contract/producer/ContractTest.java
+++ producer/src/test/java/brian/boot/example/cloud/contract/producer/ContractTest.java
@@ -1,14 +1,24 @@
 package brian.boot.example.cloud.contract.producer;
 
 import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import brian.boot.example.cloud.contract.producer.controller.CustomerController;
+import brian.boot.example.cloud.contract.producer.service.CustomerService;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes= {BootExampleCloudContractProducerApplication.class})
 public abstract class ContractTest {
+	
+	@Autowired
+	CustomerService customerService;
 
 	@Before
 	public void setup() {
-		RestAssuredMockMvc.standaloneSetup(new CustomerController());
+		RestAssuredMockMvc.standaloneSetup(new CustomerController(customerService));
 	}
 }
